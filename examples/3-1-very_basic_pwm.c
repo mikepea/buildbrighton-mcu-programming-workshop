db12f7519f328a59102e3c0b557324c0040bc5f7
Mike Pountney <Mike.Pountney@gmail.com>
2011-03-05 00:43:08 +0000

    more PWM tidy up

--- examples/3-1-very_basic_pwm.c
+++ examples/3-1-very_basic_pwm.c
@@ -6,35 +6,14 @@
  */
 
 #include <avr/io.h>
+#include "common.h"
 #include "badge.h"
 
 unsigned int loop_counter = 0;
 unsigned int second_counter = 0;
 
-void delay_ten_us(unsigned int us) {
-  unsigned int count;
-
-  while (us != 0) {
-    for (count=0; count <= 8; count++) {
-            PINB |= bogusMask;
-    }
-    us--;
-  }
-}
-
 void pre_loop_setup() {
-    // zero our timer controls, for now
-    TCCR0A = 0;
-    TCCR0B = 0;
-    TCCR1 = 0;
-    GTCCR = 0;
-
-    DDRB =  (rgbMask) | ( irOutMask );
-
-    // all PORTB output pins High (all LEDs off), except for the
-    // IR LED, which is SOURCE not SINK
-    PORTB = ( 0xFF & ~irOutMask );
-
+    initialise_registers();
 }
 
 int main(void) {
@@ -45,7 +24,7 @@ int main(void) {
 
         if ( loop_counter % 100 == 0 ) {
             TURN_RED_ON;
-        } else if ( loop_counter % 100 == 30 ) {
+        } else if ( loop_counter % 100 == 1 ) {
             TURN_RED_OFF;
         } 
 
