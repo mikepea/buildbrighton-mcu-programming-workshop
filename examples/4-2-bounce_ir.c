81c0a0fdf3eabfe0d318d3e2e2450e880487a976
Mike Pountney <Mike.Pountney@gmail.com>
2011-03-05 02:57:52 +0000

    enableIROut explained

---
+++ examples/4-2-bounce_ir.c
@@ -0,0 +1,40 @@
+/*
+ * 4-1-flash_ir.c
+ *
+ * Demonstrate receiving of IR signals from remotes
+ *
+ */
+
+#include <avr/io.h>
+#include "common.h"
+#include "recvNEC.h"
+#include "sendNEC.h"
+#include "badge.h"
+
+unsigned int loop_counter = 0;
+unsigned int second_counter = 0;
+
+void pre_loop_setup() {
+    initialise_registers();
+    enableIROut();
+}
+
+int main(void) {
+
+    pre_loop_setup();
+
+    while (1) {
+
+        if ( IR_MARK_RECEIVED ) {
+            FLASH_RED;
+        } else {
+            //FLASH_BLUE;
+        }
+        delay_ten_us(100);
+
+    }
+
+    return 0;
+
+}
+
